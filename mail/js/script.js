fadc1f5673fd3bca9a26deffa9283ff9b4aa9d12
LtMilligan <gioele.miscia@gmail.com>
2024-06-28 18:58:50 +0200

    [UPD] MAIL - Adding cycle FOR and IF for mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,3 +2,19 @@ const mailAddress = ['gioele.miscia@gmail.com', 'raffa.mancano@gmail.com', 'gioe
 
 const btnControlla = document.getElementById('controlla');
 
+let validMail = false;
+
+btnControlla.addEventListener('click', function () {
+    let userMail = document.getElementById('userMail').value.toLowerCase();
+    for (let i = 0; i < mailAddress.length; i++) {
+        if (mailAddress[i].toLowerCase() === userMail) {
+            validMail = true;
+        }
+    }
+
+    if (validMail === true) {
+        document.getElementById('validazione').innerHTML = '<h3>Complimenti, la tua mail è valida!</h3>'
+    }else {
+        document.getElementById('validazione').innerHTML = '<h3>Mi dispiace, la tua mail non è valida!</h3>'
+    }
+})
