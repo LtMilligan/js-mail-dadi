1d64819d82077cc79aea5e955b8b02a5b32df0be
LtMilligan <gioele.miscia@gmail.com>
2024-06-28 18:43:06 +0200

    [UPD] MAIL - Adding form in index and array in script with mail address

---
+++ mail/js/script.js
@@ -0,0 +1,4 @@
+const mailAddress = ['gioele.miscia@gmail.com', 'raffa.mancano@gmail.com', 'gioele.miscia@yahoo.it', 'let_vs_oni@hotmail.it']
+
+const btnControlla = document.getElementById('controlla');
+
